71e758663cdead5ac7994308581e8cbe00b823bb
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-28 16:06:41 +0500

    Create Layout component to display Header and the corresponding route content as Outlet

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+
+const Layout = () => (
+  <div className="wrapper">
+    <Header />
+    <Outlet />
+  </div>
+);
+
+export default Layout;
