0f864917c13503254e6b651ecb4cf0ce8dc71ebd
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-28 16:09:14 +0500

    Create empty MyProfile component with corresponding module css

---
+++ src/components/MyProfile.js
@@ -0,0 +1,9 @@
+import styles from '../styles/MyProfile.module.css';
+
+const MyProfile = () => (
+  <div className={styles.my_profile}>
+    This is My Profile page!
+  </div>
+);
+
+export default MyProfile;
