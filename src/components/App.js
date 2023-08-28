b13b87fec514614603733d94201c9b8f3e37cabb
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-08-28 15:33:39 +0530

    Add styles and icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import '../styles/App.css';
+import Header from './Header';
 
 const App = () => (
-  // template code to see if styles are imported correctly
-  // and ESLint is actually working, feel free to delete later
-  <h1 className="App">Example</h1>
-  // here is the space for our Routes
+  <Header />
 );
 
 export default App;
