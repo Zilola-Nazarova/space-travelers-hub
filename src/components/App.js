fdaa9a0affbdc89f772e9ffa7c36a39e3011d1e4
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-08-28 20:59:36 +0530

    Display Rockets

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,34 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import MyProfile from './MyProfile';
 import NotMatch from './NotMatch';
 import Layout from './Layout';
 import Missions from './Missions';
 import Rockets from './Rockets';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Rockets />} />
-      <Route path="missions" element={<Missions />} />
-      <Route path="profile" element={<MyProfile />} />
-      <Route path="*" element={<NotMatch />} />
-    </Route>
-  </Routes>
-);
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="profile" element={<MyProfile />} />
+          <Route path="*" element={<NotMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
