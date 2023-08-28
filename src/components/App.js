332608460a10b86c8ca0ac88307264dfac50faaf
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-28 14:18:54 +0500

    Add logger middleware to the store.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import '../styles/App.css';
 
 const App = () => (
-  <h1>Example</h1>
+  // template code to see if styles are imported correctly
+  // and ESLint is actually working, feel free to delete later
+  <h1 className="App">Example</h1>
   // here is the space for our Routes
 );
 
