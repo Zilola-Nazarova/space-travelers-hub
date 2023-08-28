80258a5d483cf419d200fc52c9a8140a121063ae
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-28 18:49:12 +0500

    Create a function to fetch data from API using axios, add extraReducers

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,13 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const MISSIONS_URL = 'https://api.spacexdata.com/v3/missions';
+
+export const getMissions = createAsyncThunk(
+  'missions/getMissions',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get(MISSIONS_URL);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
 
 const initialState = {
-  content: 'This is initial state',
+  missions: [],
+  isLoading: false,
+  error: undefined,
 };
 
 export const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: { },
+  extraReducers(builder) {
+    builder
+      .addCase(getMissions.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getMissions.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const missionsArray = [];
+        action.payload.forEach((item) => {
+          const mission = {
+            mission_id: item.mission_id,
+            mission_name: item.mission_name,
+            description: item.description,
+          };
+          missionsArray.push(mission);
+        });
+        state.missions = missionsArray;
+      })
+      .addCase(getMissions.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload.message;
+      });
+  },
 });
 
 export default missionsSlice.reducer;
